5b84a8d8730c410b0552391340eda69c7931c231
Thassya <thassya@ice.ufjf.br>
2018-06-04 18:34:26 -0300

    adicionando menu a aplicação

--- br.ufjf/main.cpp
+++ br.ufjf/main.cpp
@@ -8,6 +8,7 @@
 #include "grafo/Grafo.h"
 
 using namespace std;
+void mostrarMenuPrincipal();
 
 int main() {
 
@@ -24,6 +25,9 @@ int main() {
         printf("Erro, nao foi possivel abrir o arquivo\n");
     else
 
+
+        mostrarMenuPrincipal();
+
         fscanf(arq,"%d", &grauGrafo);
         Grafo *grafo = new Grafo(grauGrafo);
             while( (fscanf(arq,"%d %d %d\n", &v1, &v2, &p))!=EOF ) {
@@ -37,4 +41,24 @@ int main() {
     fclose(arq);
 
     return 0;
+}
+
+void mostrarMenuPrincipal() {
+    int opcao = 0;
+    while(opcao != 10){
+        cout << "Selecione uma das opções abaixo: \n";
+        cout << "01. Grau do grafo\n";
+        cout << "02. Ordem do grafo\n";
+        cout << "10. Sair\n\n";
+        cout << "Opcao escolhida: ";
+        cin >> opcao;
+    }
+
+    switch (opcao) {
+        case 2 || 02: {
+            cout << "O grafo tem a ordem -> " << g1->ordemGrafo() << " !!\n" << endl;
+            break;
+        }
+    }
+
 }
